327f6c0fe6c5d4c0ea8f33c1828e65f579a28ce1
Andrea Grippi <andrea.grippi@yahoo.it>
2024-09-16 19:55:50 +0800

    working cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 import * as functions from "firebase-functions";
 const mysql = require('mysql2/promise');
 
-exports.logData = functions.https.onRequest((req, res) => {
+/*exports.logData = functions.https.onRequest((req, res) => {
     // Log the received data to the console
     console.log('Received data:', req.body);
 
     // Optionally, you can return a response
     res.status(200).send({ message: 'Data logged successfully!' });
-});
+});*/
 
 const INSTANCE_CONNECTION_NAME = 'epago-b4676:us-central1:mqtt-db-test';
 
@@ -24,22 +24,38 @@ const pool = mysql.createPool({
   socketPath: `/cloudsql/${INSTANCE_CONNECTION_NAME}`,
 });
 
+
+
 exports.writeJsonToDatabase = functions.https.onRequest(async (req, res) => {
   try {
-    const jsonData = req.body; // Assuming JSON data is sent in the request body
+      // Parse the JSON body
+      let jsonData = JSON.parse(req.body);
 
-    // Example: Assuming jsonData has fields 'name' and 'value'
-    const { name, value } = jsonData;
+      const sensor1Data = jsonData.sensor1;
+      const sensor2Data = jsonData.sensor2;
+      jsonData.topic = "test/topic"
 
-    const connection = await pool.getConnection();
-    await connection.query('INSERT INTO your_table_name (name, value) VALUES (?, ?)', [name, value]);
-    connection.release();
+      // Obtain the current Unix timestamp
+      //const timestamp = Date..toLocaleString(); // Unix timestamp in seconds
+      let date = new Date();
+      const datetime = date.toISOString().split('T')[0] + ' '
+        + date.toTimeString().split(' ')[0];
+      const connection = await pool.getConnection();
 
-    res.status(200).send('Data written to database successfully');
+      // Insert data for sensor 1
+      await connection.query('INSERT INTO sensor_data (sensor_name, temperature, humidity, topic, timestamp) VALUES (?,?,?,?,?)', 
+          ['sensor1', sensor1Data.t, sensor1Data.h, jsonData.topic, datetime]);
+      
+      // Insert data for sensor 2
+      await connection.query('INSERT INTO sensor_data (sensor_name, temperature, humidity, topic, timestamp) VALUES (?,?,?,?,?)', 
+          ['sensor2', sensor2Data.t, sensor2Data.h, jsonData.topic, datetime]);
+
+      connection.release();
+
+      res.status(200).send('Data written to database successfully');
   } catch (error) {
-    console.error('Error writing to database:', error);
-    //res.status(500).send('Error writing to database');
-    res.status(500).send(error);
-
+      console.log(req.body);
+      console.error('Error writing to database:', error);
+      res.status(500).send(error);
   }
 });
